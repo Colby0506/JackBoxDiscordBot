f0aa34cc8254b222dc124f0f22607be50043fa3d
Colby0506 <Colby0506@hotmail.com>
2022-07-29 20:33:09 -0400

    Day 1

--- app/Program.cs
+++ app/Program.cs
@@ -1,9 +1,58 @@
 ﻿using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
 namespace Program{
-    class main{
-        static void Main(string[] args){
-            Console.WriteLine("hello World");
-            Console.ReadKey();
+    class app{
+        public static Task Main(string[] args) => new app().MainAsync();
+        private DiscordSocketClient _client;
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.MessageReceived += CommandHandler;
+            _client.Log += Log;
+            var token = "MTAwMjcyMzUxMjE0ODAzMzU5OQ.GFSfpu.MsOA1fHWUVcxT2FwPqdif_SoCHPXBYbhLm4C1I";
+            await _client.LoginAsync(TokenType.Bot,token);
+            await _client.StartAsync();
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        private Task CommandHandler(SocketMessage message)
+        {
+            string command = "";
+            int lengthofcommand = -1;
+            
+            //filter
+
+            if(!message.Content.StartsWith("^"))
+                return Task.CompletedTask;
+
+            if (message.Author.IsBot)
+                return Task.CompletedTask;
+
+            if (message.Content.Contains(" "))
+            {
+                lengthofcommand = message.Content.IndexOf(" ");
+            }
+            else
+            {
+                lengthofcommand = message.Content.Length;
+            }
+            command = message.Content.Substring(1,lengthofcommand-1);
+            
+            //commands
+            if (command.Equals("hi"))
+            {
+                message.Channel.SendMessageAsync($@"Hello {message.Author.Mention}" );
+            }
+
+
+            return Task.CompletedTask;
         }
     }
 }
